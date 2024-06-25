cceef9b4bb539fa4c2d16628319020d02dbdca48
carlos676767 <carlitobr730@gmail.com>
2024-06-25 12:20:11 -0700

    finalindo o cadastro.

---
+++ barbearia/public/backend/db/db.js
@@ -0,0 +1,6 @@
+
+
+const dadosDatabase = async() => {
+
+}
+
---
+++ barbearia/public/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const api = express();
+
+const consign = require("consign")
+
+consign()
+.include("./routers")
+.into(api);
+
+
+const port = 8080 | process.env.PORT;
+api.listen(8080, () => {
+  console.log(`serving ${port}`);
+});
