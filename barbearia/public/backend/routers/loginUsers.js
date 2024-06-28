270e7aeafd1e01b41fdf65c65b8fdf3a518d9184
carlos676767 <carlitobr730@gmail.com>
2024-06-28 09:25:49 -0700

    codigos organiados ok.

--- barbearia/public/backend/routers/loginUsers.js
+++ barbearia/public/backend/routers/loginUsers.js
@@ -6,10 +6,10 @@ module.exports = (api) => {
       const { email, senha } = res.body;
       if (!email && !senha) {
         data.send({message: "Os campo é obrigatório e não pode ficar vazio.", status: 400, }).status(400);
-        return;
+        return; 
       }
 
-      await dbLoginUser(email,senha)
+       
       data.send({message: "a", status: 200, })
     } catch (error) {
       data.send({message: "ocorreu um erro inesperado, tente novamente mais tarde.", status: 404}).status(404)
--- barbearia/public/backend/routers/userCadastro.js
+++ barbearia/public/backend/routers/userCadastro.js
@@ -10,9 +10,9 @@ module.exports = (api) => {
   api.post("/registro", async (res, data) => {
     try {
       const { nome, email, idade, senha } = res.body;
-      validarEmail(email, data)
-      validarSenha(senha, data)
-      await validarCamposObrigarios(nome, email, idade, senha, data)
+      validarEmail(email, data);
+      validarSenha(senha, data);
+      await validarCamposObrigarios(nome, email, idade, senha, data);
     } catch (error) {
       data.send({ message: "servidor nao encontrou ou deu um erro, tente novamente mais tarde.", status: 404, }).status(404);
     }
@@ -22,15 +22,15 @@ module.exports = (api) => {
 const validarEmail = (email, data) => {
   const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!regexEmail.test(email)) {
-    data.send({ message: "Digite um email valido, obrigado.", status: 400 }).status(400)
-    return
+    data.send({ message: "Digite um email valido, obrigado.", status: 400 }).status(400);
+    return;
   }
 }
 
 const validarSenha = (senha, data) => {
   if (senha.length < 8) {
-    data.send({ message: "Sua senha deve ter mais de oito caracteres", status: 400 }).status(400)
-    return
+    data.send({ message: "Sua senha deve ter mais de oito caracteres", status: 400 }).status(400);
+    return;
   }
 }
 
@@ -38,8 +38,8 @@ const validarCamposObrigarios = async(nome, email, idade, senha, data) => {
   if (!nome && !email && !idade & !senha) {
     data.send({ message: "Os campo é obrigatório e não pode ficar vazio.", status: 400, }).status(400);
   } else {
-    await databaseRegisterUsers(nome, email, idade, senha)
-    const token = jwt.sign({ nome, email, idade, senha }, SECRET_KEY, { expiresIn: "1h" })
+    await databaseRegisterUsers(nome, email, idade, senha);
+    const token = jwt.sign({ nome, email, idade, senha }, SECRET_KEY, { expiresIn: "1h" });
     data.send({ message: "Dados recebidos", tk: token, status: 200, registro: true, }).status(200);
   }
 }
