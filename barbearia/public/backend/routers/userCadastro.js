e368fa9f9b1569428edce954097d59cd2ebdebbb
carlos676767 <carlitobr730@gmail.com>
2024-06-28 08:06:29 -0700

    validar senha ok.

--- barbearia/public/backend/routers/userCadastro.js
+++ barbearia/public/backend/routers/userCadastro.js
@@ -4,33 +4,37 @@ const crypetor = require("crypto")
 const randomPassWorldKey = crypetor.randomBytes(32).toString("hex")
 const SECRET_KEY = randomPassWorldKey
 module.exports = (api) => {
-    const bodyMiddle = require("body-parser");
-    api.use(bodyMiddle.json());
-    api.post("/registro", async (res, data) => {
-      try {
-        const { nome,email, idade, senha } = res.body;
-        validarEmail(email, data)
-        if (senha.length < 8) {
-          data.send({message: "Sua senha deve ter mais de oito caracteres", status: 400}).status(400)
-          return
-        }
-        if(!nome && !email && !idade & !senha) {
-         data.send({message: "Os campo é obrigatório e não pode ficar vazio.", status: 400,  }).status(400); 
-        }else{
-        await databaseRegisterUsers(nome,email, idade, senha)
-        const token = jwt.sign({nome, email, idade, senha}, SECRET_KEY, {expiresIn: "1h"})
-        data.send({  message: "Dados recebidos",tk: token, status: 200, registro: true, }).status(200);
-        }
-      } catch (error) {
-        data.send({message:"servidor nao encontrou ou deu um erro, tente novamente mais tarde.",status: 404, }).status(404);
+  const bodyMiddle = require("body-parser");
+  api.use(bodyMiddle.json());
+  api.post("/registro", async (res, data) => {
+    try {
+      const { nome, email, idade, senha } = res.body;
+      validarEmail(email, data)
+      validarSenha(senha, data)
+      if (!nome && !email && !idade & !senha) {
+        data.send({ message: "Os campo é obrigatório e não pode ficar vazio.", status: 400, }).status(400);
+      } else {
+        await databaseRegisterUsers(nome, email, idade, senha)
+        const token = jwt.sign({ nome, email, idade, senha }, SECRET_KEY, { expiresIn: "1h" })
+        data.send({ message: "Dados recebidos", tk: token, status: 200, registro: true, }).status(200);
       }
-    });
+    } catch (error) {
+      data.send({ message: "servidor nao encontrou ou deu um erro, tente novamente mais tarde.", status: 404, }).status(404);
+    }
+  });
 }
 
 const validarEmail = (email, data) => {
   const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!regexEmail.test(email)) {
     data.send({ message: "Digite um email valido, obrigado.", status: 400 }).status(400)
+    return
+  }
+}
+
+const validarSenha = (senha, data) => {
+  if (senha.length < 8) {
+    data.send({ message: "Sua senha deve ter mais de oito caracteres", status: 400 }).status(400)
     return
   }
 }
